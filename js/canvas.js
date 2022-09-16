fc72f87db5cb5e73fb5e70eefc96d82e3970456e
Aécio de Oliveira <aecio.programmer@gmail.com>
2022-09-16 15:45:17 -0300

    Initial commit Jogo da Forca project

---
+++ js/canvas.js
@@ -0,0 +1,137 @@
+//Define variáveis globais
+const gameArea = document.querySelector("canvas").getContext("2d");
+
+//Desenha a área do jogo
+//Define o padrão das linhas
+function showCanvas() {
+  gameArea.lineWidth = 6;
+  gameArea.lineCap = "round";
+  gameArea.lineJoin = "round";
+  gameArea.fillStyle = "#E5E5E5";
+  gameArea.strokeStyle = "#0A3871";
+  gameArea.fillRect(0, 0, 1200, 800);
+  drawGallows();
+}
+
+//Desenha as linhas correspondentes ao tamanho da palavra
+function drawLines(lines) {
+  //Define o início do desenho das linhas centralizando de acordo com
+  //o tamanho definido para a área do jogo
+  //80 é a largura da linha e 100 o espaçamento entre início de cada linha
+
+  const initialXCoordinate =
+    (1200 - (80 * lines + (100 - 80) * (lines - 1))) / 2;
+  for (let i = 0; i < lines; i++) {
+    gameArea.beginPath();
+    gameArea.moveTo(initialXCoordinate + i * 100, 560);
+    gameArea.lineTo(initialXCoordinate + 80 + i * 100, 560);
+    gameArea.stroke();
+  }
+}
+
+//Desenha as letras corretas na área do jogo
+function drawRightLetter(lines, letter, arrayLetterPosition) {
+  //Define o início do desenho das linhas centralizando de acordo com
+  //o tamanho definido para a área do jogo
+  //80 é a largura da linha e 100 o espaçamento entre início de cada linha
+  const initialXCoordinate =
+    (1200 - (80 * lines + (100 - 80) * (lines - 1))) / 2;
+  gameArea.fillStyle = "#0A3871";
+  gameArea.font = "72px Inter";
+  gameArea.fillText(
+    letter,
+    initialXCoordinate + 20 + arrayLetterPosition * 100,
+    520
+  );
+}
+
+//Desenha as letras erradas na área do jogo
+function drawWrongLetter(letters, keys, color = "#0A3871") {
+  const initialXCoordinate = (1200 - 20 * letters) / 2;
+  gameArea.fillStyle = color;
+  gameArea.font = "24px Inter";
+  gameArea.fillText(keys, initialXCoordinate, 620);
+}
+
+//Limpa a área das letras erradas
+function clearWrongLettersArea() {
+  gameArea.clearRect(0, 600, 1200, 640);
+}
+
+//Desenha a mensagem de fim de jogo
+function drawEndGameMessage(text, color) {
+  gameArea.fillStyle = color;
+  gameArea.font = "32px Inter";
+  gameArea.fillText(text, 780, 220);
+}
+
+//Desenha a forca
+function drawGallows() {
+  gameArea.beginPath();
+  gameArea.moveTo(450, 380);
+  gameArea.lineTo(750, 380);
+  gameArea.stroke();
+  gameArea.moveTo(520, 380);
+  gameArea.lineTo(520, 5);
+  gameArea.stroke();
+  gameArea.moveTo(520, 5);
+  gameArea.lineTo(700, 5);
+  gameArea.stroke();
+  gameArea.moveTo(700, 5);
+  gameArea.lineTo(700, 50);
+  gameArea.stroke();
+}
+
+//Desenha a cabeça
+function drawHead() {
+  gameArea.beginPath();
+  gameArea.arc(700, 80, 30, 0, 2 * Math.PI);
+  gameArea.stroke();
+}
+
+//Desenha o braço direito
+function drawRightArm() {
+  gameArea.beginPath();
+  gameArea.moveTo(700, 110);
+  gameArea.lineTo(740, 180);
+  gameArea.stroke();
+}
+
+//Desenha o braço esquerdo
+function drawLeftArm() {
+  gameArea.beginPath();
+  gameArea.moveTo(700, 110);
+  gameArea.lineTo(660, 180);
+  gameArea.stroke();
+}
+
+//Desenha o corpo
+function drawBody() {
+  gameArea.beginPath();
+  gameArea.moveTo(700, 110);
+  gameArea.lineTo(700, 260);
+  gameArea.stroke();
+}
+
+//Desenha a perna direita
+function drawRightLeg() {
+  gameArea.beginPath();
+  gameArea.moveTo(700, 260);
+  gameArea.lineTo(740, 330);
+  gameArea.stroke();
+}
+
+//Desenha a perna esquerda
+function drawLeftLeg() {
+  gameArea.beginPath();
+  gameArea.moveTo(700, 260);
+  gameArea.lineTo(660, 330);
+  gameArea.stroke();
+}
+
+function drawLossHead() {
+  gameArea.fillStyle = "#0A3871";
+  gameArea.font = "24px Inter";
+  gameArea.fillText("+", 680, 80);
+  gameArea.fillText("+", 705, 80);
+}
---
+++ js/forca.js
@@ -0,0 +1,257 @@
+//Define as variáveis globais
+const mainContent = document.querySelector("#main-section");
+const gameSection = document.querySelector("#game-section");
+const wordSection = document.querySelector("#word-section");
+
+//Array que armazena as palavras secretas digitadas
+const secretWord = ["PALAVRA", "PROBLEMA", "MARAVILHA", "MORCEGO", "CARAMUJO"];
+
+// const secretWord = [];
+
+//Variável que armazena a palavra secreta sorteada
+let randomSecretWord;
+
+//Variável que armazena as letras corretas digitadas na ordem correta
+//em que estão na palavra secreta sorteada
+let guessSecretWord = [];
+
+//Array que armazena as letras erradas digitadas
+let wrongGuesses = [];
+
+//Função de início do script
+function start() {
+  console.log("Iniciando o jogo da forca...");
+  startGame();
+  addWord();
+  saveAndBegin();
+  cancelGame();
+  newGame();
+  // inputListener();
+}
+
+//Inicia o jogo
+function startGame() {
+  const startGameButton = document.querySelector("#startGameButton");
+
+  startGameButton.addEventListener("click", function () {
+    if (secretWord.length > 0) {
+      mainContent.classList.add("hide-section");
+      gameSection.classList.remove("hide-section");
+      showCanvas();
+      randomWord();
+    } else {
+      handleAddWordButton();
+      alert("Não há palavras secretas digitadas ainda! Digite uma....");
+    }
+  });
+}
+
+//Ativa a tela de inclusão de palavras
+function addWord() {
+  const addWordButton = document.querySelector("#addNewWordButton");
+  addWordButton.addEventListener("click", handleAddWordButton);
+}
+
+function handleAddWordButton() {
+  const word = document.getElementById("secret-word");
+
+  mainContent.classList.add("hide-section");
+  wordSection.classList.remove("hide-section");
+
+  word.value = "";
+  word.focus();
+}
+
+// function addWord() {
+//   const addWordButton = document.querySelector("#addNewWordButton");
+//   const word = document.getElementById("secret-word");
+//   word.focus();
+
+//   addWordButton.addEventListener("click", function () {
+//     mainContent.classList.add("hide-section");
+//     wordSection.classList.remove("hide-section");
+//     word.value = "";
+//   });
+// }
+
+//Salva a palavra digitada no array de palavras e inicia o jogo
+
+function saveAndBegin() {
+  const saveWordButton = document.querySelector("#saveWordButton");
+  saveWordButton.addEventListener("click", function () {
+    const word = document.querySelector("#secret-word");
+    const wordValue = word.value.toUpperCase();
+
+    //Verifica se a palavra digitada está conforme as regras, armazena a palavra no array de
+    //palavras secretas, ativa a tela do jogo e inicia o jogo
+    if (inputKeyCheck(wordValue)) {
+      word.value = wordValue;
+      !secretWord.includes(wordValue)
+        ? secretWord.push(wordValue)
+        : console.log("Palavra já inserida!");
+      wordSection.classList.add("hide-section");
+      gameSection.classList.remove("hide-section");
+
+      showCanvas();
+      randomWord();
+    } else {
+      word.value = "";
+      alert("Somente letras sem acento são permitidas! Tente novamente...");
+    }
+  });
+}
+
+//Define o listener dos botões de Cancelar nas telas de Adicionar Palavra e do Jogo
+function cancelGame() {
+  const cancelGameButton = document.querySelector("#cancelGameButton");
+  const cancelButton = document.querySelector("#cancelButton");
+  cancelGameButton.addEventListener("click", handleCancelButton);
+  cancelButton.addEventListener("click", handleCancelButton);
+}
+
+//Ativa a exibição da tela principal e desativa das demais
+function handleCancelButton() {
+  gameSection.classList.add("hide-section");
+  wordSection.classList.add("hide-section");
+  mainContent.classList.remove("hide-section");
+}
+
+//Reinicia o jogo
+function newGame() {
+  const newGameButton = document.querySelector("#newGameButton");
+  newGameButton.addEventListener("click", function () {
+    showCanvas();
+    resetGame();
+    randomWord();
+  });
+}
+
+//Sorteia a palavra secreta, chama a função de desenho das linhas
+//e ativa o listener do keyboard
+
+function randomWord() {
+  let randomNumber = Math.floor(Math.random() * secretWord.length);
+  randomSecretWord = secretWord[randomNumber];
+
+  drawLines(randomSecretWord.length);
+  keyboardListener();
+}
+
+//Define o listener do Keyboard
+function keyboardListener() {
+  document.addEventListener("keypress", handleKeybordListener);
+}
+
+//Função do listener do keyboard
+function handleKeybordListener(Event) {
+  const keyPressed = Event.key.toUpperCase();
+
+  //Verifica se a letra do palpite está na palavra, armazena no array próprio
+  //o palpite errado e chama a função de desenho das letras erradas
+
+  if (inputKeyCheck(keyPressed)) {
+    if (guessCheck(keyPressed) && !wrongGuesses.includes(keyPressed)) {
+      wrongGuesses.push(keyPressed);
+      clearWrongLettersArea();
+      drawWrongLetter(wrongGuesses.length, wrongGuesses.join(" "));
+
+      //Verifica se o jogo terminou
+      endGame();
+
+      //Chama a função de desenho do boneco da forca de acordo com o número de erros
+      //expresso pelo tamanho do array que armazena os palpites errados
+      switch (wrongGuesses.length) {
+        case 1:
+          drawHead();
+          break;
+        case 2:
+          drawBody();
+          break;
+        case 3:
+          drawRightArm();
+          break;
+        case 4:
+          drawLeftArm();
+          break;
+        case 5:
+          drawRightLeg();
+          break;
+        case 6:
+          drawLeftLeg();
+          break;
+      }
+    }
+  } else {
+    alert("Somente letras sem acento são permitidas! Tente novamente...");
+  }
+}
+
+// function inputListener() {
+//   const wordInput = document.querySelector("#secret-word");
+//   wordInput.addEventListener('keypress')
+// }
+
+//Reinicializa as variáveis globais e remove o listener para evitar duplicidade
+//de letras erradas
+function resetGame() {
+  wrongGuesses = [];
+  randomSecretWord = "";
+  guessSecretWord = [];
+  document.removeEventListener("keypress", handleKeybordListener);
+}
+
+//Verifica se a letra escolhida está dentro da regra das letras permitidas no jogo
+function inputKeyCheck(text) {
+  const inputRule = /[A-Z]/g;
+  return inputRule.test(text);
+}
+
+//Verifica se a letra escolhida está correta
+function guessCheck(guess) {
+  let guessCheckError = true;
+
+  //Itera a palavra secreta e verifica se a letra escolhida pertence
+  //à palavra
+  for (let i = 0; i < randomSecretWord.length; i++) {
+    if (guess === randomSecretWord[i]) {
+      guessCheckError = false;
+
+      //Armazena a letra do palpite no array na posição correta
+      guessSecretWord[i] = guess;
+
+      //Chama a função para escrever as letras corretas na posição correta
+      drawRightLetter(randomSecretWord.length, guess, i);
+
+      //Verifica se o jogador venceu
+
+      endGame();
+    }
+  }
+  return guessCheckError;
+}
+
+//Verifica se o jogador venceu ou perdeu o jogo
+function endGame() {
+  let endGameMessage;
+  console.log("Palavra: ", randomSecretWord);
+
+  //Verifica se o tamanho do array que armazena os palpites errados chegou ao limite
+  if (wrongGuesses.length === 6) {
+    endGameMessage = "Você Perdeu! Fim de Jogo!";
+    drawEndGameMessage(endGameMessage, "red");
+    drawLossHead();
+    document.removeEventListener("keypress", handleKeybordListener);
+  }
+
+  //Verifica se o tamanho do array que armazena os palpites corretos
+  //é igual ao tamanho da palavra sorteada
+
+  if (guessSecretWord.toString().replaceAll(",", "") === randomSecretWord) {
+    endGameMessage = "Você Venceu!";
+    drawEndGameMessage(endGameMessage, "green");
+  }
+
+  //Chama a função que desenha a mensagem de fim de jogo na tela
+}
+
+start();
